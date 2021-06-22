2f0b87f52bf611c350b41403902530dc3b7e476f
Matheus Maciel de Sales <mmselas02@gmail.com>
2021-06-22 09:41:19 -0300

    site1

--- ex07/site1/script.js
+++ ex07/site1/script.js
@@ -1,1 +1,19 @@
-alert("Olá!")
+function carregar() {
+    var msg = window.document.getElementById("mensagem")
+    var img = window.document.getElementById("imagem")
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    if (hora >= 0 && hora < 12){
+            img.src = 'img_manha.png'
+            document.body.style.background = "#feb98e"
+    }       else if (hora>= 12 && hora <18){
+            img.src = 'img_tarde.png'
+            document.body.style.background = '#fdde81'
+    }
+        else {
+            img.src = 'img_noite.png'
+            document.body.style.background = '#223442'
+    }
+}
