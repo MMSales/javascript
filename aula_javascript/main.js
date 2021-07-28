d670bb37eb1e20d7a889c60a496c7522fc825ffb
MMSales <mmselas02@gmail.com>
2021-07-28 12:52:05 -0300

    aulas 1-3

---
+++ aula_javascript/main.js
@@ -0,0 +1,67 @@
+/*Aula 3
+
+
+var d = new Date()
+alert(d.getMonth() + 1)
+
+
+
+var count 
+for(count=0;count <= 5;count++){
+    alert(count)
+}
+
+
+
+var count = 0
+while (count < 5){
+    console.log(count)
+    count++
+}
+
+
+
+
+
+var idade = prompt("Digite sua idade")
+if (idade >= 18) {
+    alert("Maior de idade")
+}else{
+    alert("Menor de idade")
+}
+*/
+
+
+
+
+/*AULA 2
+
+var frutas = [{nome: "maçã", cor:"vermelha"}, {nome: "uva" , cor:  "roxa"}]
+alert(frutas[1].nome)
+var fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta)
+console.log(fruta.nome)
+var lista = ["maçã", "pêra" , "laranja"]
+//lista começa do índice 0
+lista.push("uva")
+lista.pop() //tira o último elemento da lista
+console.log(lista)
+console.log(lista.length)
+console.log(lista.reverse())
+console.log(lista.toString())
+console.log(lista.join(" - "))*/
+                       
+                 
+/*AULA 1
+var nome = "Matheus Maciel";
+var frase = "Japão é o melhor país do mundo"
+var idade = 20
+var idade2 = 33
+alert(nome + " tem " + idade + " anos.")
+alert(idade+idade2);  
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase())
+alert(frase.toLowerCase())*/
