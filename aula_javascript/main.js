731843e275c239db01170e14b9139d41112766ba
MMSales <mmselas02@gmail.com>
2021-07-29 13:56:38 -0300

    Aulas 4-5
    Curso de Programação para internet com JavaScript terminado.

--- aula_javascript/main.js
+++ aula_javascript/main.js
@@ -1,3 +1,60 @@
+function botao() {
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar no botão!<b>"
+   
+}
+
+function redirecionar() {
+    //abre página diferente
+    window.open("https://www.youtube.com/")
+    //abre na mesma página
+    window.location.href = "https://www.youtube.com/"
+}
+
+function trocar(elemento) {
+    //document.getElementById("mouse_move").innerHTML = "Obrigado por passar o mouse"
+    elemento.innerHTML = "Obrigado por passar o mouse."
+}
+
+function voltar(elemento) {
+    elemento.innerHTML = "Passe o mouse aqui."
+   
+}
+
+function load() {
+    alert("Página carregada")
+}
+
+function change(elemento){
+    console.log(elemento.value)
+}
+
+
+
+
+
+
+
+
+/*AULA 4
+function soma(n1,n2) {
+    return n1 + n2
+}
+
+function validadeIdade(idade) {
+    var validar
+    if (idade >= 18){
+        validar = true
+    }else {
+        validar = false
+    }
+    return validar
+}
+
+var idade = prompt("Digite sua idade.")
+console.log(validadeIdade(idade))
+
+alert(soma(5,10))*/
+
 /*Aula 3
 
 
